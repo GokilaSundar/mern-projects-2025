9524a5405a344db82bc8bd0f5e1f36cd009d87ae
GokilaSundar <gokithilaga267@gmail.com>
2025-01-21 23:47:06 +0530

    Implement APIs for CRUD operations in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import express from "express";
 import mongoose from "mongoose";
 import { join } from "path";
 
+import { Book } from "./models/Book.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -13,6 +15,88 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.post("/api/books", async (req, res) => {
+  const { title, author, description, year, price } = req.body;
+
+  const book = new Book({
+    title,
+    author,
+    description,
+    year,
+    price,
+  });
+
+  const createdBook = await book.save();
+
+  res.status(201).json(createdBook);
+});
+
+app.put("/api/books/:id", async (req, res) => {
+  const book = await Book.findById(req.params.id);
+
+  if (book) {
+    const { title, author, description, year, price } = req.body;
+
+    book.title = title;
+    book.author = author;
+    book.description = description;
+    book.year = year;
+    book.price = price;
+
+    const updatedBook = await book.save();
+
+    res.json(updatedBook);
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
+});
+
+app.delete("/api/books/:id", async (req, res) => {
+  const book = await Book.findById(req.params.id);
+
+  if (book) {
+    await book.deleteOne();
+    res.json({ message: "Book removed" });
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
+});
+
+app.get("/api/books", async (req, res) => {
+  const search = req.query.search;
+
+  let findOptions = {};
+
+  if (search) {
+    if (isNaN(search)) {
+      findOptions = {
+        $or: [
+          { title: { $regex: search, $options: "i" } },
+          { author: { $regex: search, $options: "i" } },
+          { description: { $regex: search, $options: "i" } },
+        ],
+      };
+    } else {
+      const numberValue = Number(search);
+      const searchRegex = new RegExp(search, "i");
+
+      findOptions = {
+        $or: [
+          { title: searchRegex },
+          { author: searchRegex },
+          { description: searchRegex },
+          { year: numberValue },
+          { price: numberValue },
+        ],
+      };
+    }
+  }
+
+  const books = await Book.find(findOptions);
+
+  res.json(books);
+});
 
 async function main() {
   // #region For production deployment with frontend
---
+++ backend/src/models/Book.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+});
+
+export const Book = mongoose.model('Book', bookSchema, 'books');
