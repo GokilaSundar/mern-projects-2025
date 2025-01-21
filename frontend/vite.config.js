81d17b3ef6453ef1ba1162c12a41ae847b944941
GokilaSundar <gokithilaga267@gmail.com>
2025-01-21 23:15:03 +0530

    Use vite to proxy API calls instead of setting default baseURL in axios in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { config } from "dotenv";
+import { defineConfig } from "vite";
+
+config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // eslint-disable-next-line no-undef
+      "/api": process.env.API_BASE_URL,
+    },
+  },
+});
