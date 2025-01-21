7f35f2c12712af078b2faf367aa810b9475337e0
GokilaSundar <gokithilaga267@gmail.com>
2025-01-21 21:59:08 +0530

    Create basic server with mongoose connection in backend

---
+++ backend/src/index.js
@@ -0,0 +1,33 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("API is running...");
+});
+
+async function main() {
+  try {
+    console.log("Connecting to MongoDB...");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+main().catch((err) => {
+  console.error("Failed to start server", err);
+  process.exit(1);
+});
