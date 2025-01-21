266117b058fb5bf1b4ca16d0f09f25cfa38e858d
GokilaSundar <gokithilaga267@gmail.com>
2025-01-21 22:21:10 +0530

    Configure base URL for API calls in frontend

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,8 @@
+import axios from "axios"
 import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 
 createRoot(document.getElementById("root")).render(<App />);
